b8e20ef9560f4f22dafe6301d0a4d9f56448a255
mautini <mautini@users.noreply.github.com>
2018-02-02 21:16:30 +0100

    Add Jsoup and structure for parsing

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,10 +1,24 @@
+import com.google.schemaorg.core.Thing;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scraper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Scraper.class);
 
     public Scraper() {
+    }
+
+    public List<Thing> extract(File file) throws IOException {
+        Document document = Jsoup.parse(file, "UTF-8");
+
+        return new ArrayList<>();
     }
 }
