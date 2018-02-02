2e38a527e5491085c3e5658838f675abb1ba692d
mautini <mautini@users.noreply.github.com>
2018-02-02 21:07:45 +0100

    Add logger (slf4j + log4j2)

---
+++ src/main/java/Scraper.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Scraper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Scraper.class);
+
+    public Scraper() {
+    }
+}
